b30a7c7da950a94102277bc707ac1e852ac0bd9b
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-12-15 10:53:02 +0200

    created a form for logIn

--- courseApplication/frontend/src/components/LogIn.js
+++ courseApplication/frontend/src/components/LogIn.js
@@ -1,9 +1,30 @@
 const LogIn = () =>{
+    const submitLogInForm = (event) => {
+      event.preventDefault()
+      const username = event.target.username.value
+      const password = event.target.password.value
+      
+
+
+
+    }
+
+
     return(
-      <>
+    <>
       <p>Log in page</p>
-      </>
   
+
+      <form onSubmit={submitLogInForm}>
+        <input type = "text" name = "username"></input>
+        <br></br>
+        <input type="password" name="password"></input>
+        <br></br>
+        <input type = "submit" value="LogIn"></input>
+      </form>
+
+
+    </>
     )
   }
   export default LogIn
