a80ebe9906b639f024c0c05afed0c383602c3aad
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2022-12-15 11:10:47 +0200

    created a mutation to frontend for logIn

---
+++ courseApplication/frontend/src/queries/userQueries.js
@@ -0,0 +1,10 @@
+import { gql } from '@apollo/client'
+
+
+const LOGIN = gql`
+mutation LogIn($username: String!, $password: String!) {
+    logIn(username: $username, password: $password) {
+      value
+    }
+  }
+`
