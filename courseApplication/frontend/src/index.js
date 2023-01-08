88fe76f0017a9f6c1ead14cd2df9a2fb73a2e274
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-08 16:15:57 +0200

    moved apolloclient creation to client.js from index.js

--- courseApplication/frontend/src/index.js
+++ courseApplication/frontend/src/index.js
@@ -1,24 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-import {
-  ApolloClient,
-  ApolloProvider,
-  HttpLink,
-  InMemoryCache,
-} from '@apollo/client'
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'http://localhost:4000',
-  }),
-})
-
-
-
+import client from './client';
+import {  ApolloProvider } from '@apollo/client'
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
    <ApolloProvider client={client}>
         <App />
