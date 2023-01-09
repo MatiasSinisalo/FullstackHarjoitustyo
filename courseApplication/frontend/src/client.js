c4f5af3a2bb417c14263b7afacfa42fc9c8fbd3f
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-09 16:03:53 +0200

    added token to header if the token is found from local storage

--- courseApplication/frontend/src/client.js
+++ courseApplication/frontend/src/client.js
@@ -3,12 +3,27 @@ import {
     HttpLink,
     InMemoryCache,
   } from '@apollo/client'
-  
+import { setContext } from '@apollo/client/link/context'
+
+const httpLink = new HttpLink({
+    uri: 'http://localhost:4000',
+})
+
+const tokenHeader = setContext((_, { headers }) => {
+    const token = localStorage.getItem('courseApplicationUserToken')
+    if(token)
+    {
+        return {headers: {...headers, authorization: `bearer: ${token}`}}
+    }
+    else
+    {
+        return {headers: {...headers, authorization: null}}
+    }
+})
+
 const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: new HttpLink({
-        uri: 'http://localhost:4000',
-    }),
+    link: tokenHeader.concat(httpLink)
 })
 
 export default client
