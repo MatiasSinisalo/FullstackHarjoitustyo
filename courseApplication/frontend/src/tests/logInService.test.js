6fdc4180e2602f8832c78b0231f62d4d6c8a2bb7
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-09 13:38:24 +0200

    added test to check that the useUserLogIn hook returns a token if login mutation succeeds

--- courseApplication/frontend/src/tests/logInService.test.js
+++ courseApplication/frontend/src/tests/logInService.test.js
@@ -1,10 +1,29 @@
 import { useUserLogIn } from "../services/logInService"
 
-import '@testing-library/jest-dom/extend-expect'
 
 
-test('login component shows login form correctly', () => {
-    const getToken = useUserLogIn()
+test('useUserLogIn hook returns token returned by successfull mutate query', async () => {
+    
+    const successfullResult = {
+        data: {
+            logIn: "abc1234"
+        }
+    }
 
+    const mockClient = {
+        mutate : (data) => {
+            if(data.variables.username == "username" & data.variables.password == "password"){
+                return successfullResult
+            }
+            else
+            {
+                return null
+            }
+        }
+    }
+     
+    const getToken = useUserLogIn(mockClient)
+    const token = await getToken("username", "password")
+    expect(token).toBe('abc1234')
 })
 
