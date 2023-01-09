4edfb47250b976558c92a78fa9d9b823e0779387
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-09 13:45:02 +0200

    added test to check that hook useUserLogIn returns undefined if credentials are incorrect

--- courseApplication/frontend/src/tests/logInService.test.js
+++ courseApplication/frontend/src/tests/logInService.test.js
@@ -1,29 +1,38 @@
 import { useUserLogIn } from "../services/logInService"
 
+const successfullResult = {
+    data: {
+        logIn: "abc1234"
+    }
+}
+
+const failedResult = {
+    data: {
+        logIn: null
+    }
+}
+
+const mockClient = {
+    mutate : (data) => {
+        if(data.variables.username == "username" & data.variables.password == "password"){
+            return successfullResult
+        }
+        else
+        {
+            return failedResult
+        }
+    }
+}
 
 
 test('useUserLogIn hook returns token returned by successfull mutate query', async () => {
-    
-    const successfullResult = {
-        data: {
-            logIn: "abc1234"
-        }
-    }
-
-    const mockClient = {
-        mutate : (data) => {
-            if(data.variables.username == "username" & data.variables.password == "password"){
-                return successfullResult
-            }
-            else
-            {
-                return null
-            }
-        }
-    }
-     
     const getToken = useUserLogIn(mockClient)
     const token = await getToken("username", "password")
     expect(token).toBe('abc1234')
 })
 
+test('useUserLogIn hook returns null if mutation query fails', async () => {
+    const getToken = useUserLogIn(mockClient)
+    const token = await getToken("incorrect", "incorrect password")
+    expect(token).toBeUndefined()
+})
