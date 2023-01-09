ff4718104dd44805b5340993de102b99b0610db8
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-09 13:51:18 +0200

    removed console.logs from logInService

--- courseApplication/frontend/src/services/logInService.js
+++ courseApplication/frontend/src/services/logInService.js
@@ -6,13 +6,11 @@ export const useUserLogIn = (apolloClient) => {
     
     const getToken = async (username, password) => {
         const result = await appClient.mutate({mutation: LOGIN, variables: {username, password}})
-        console.log(result)
         if(result.data.logIn)
         {
-            console.log("logged in")
             return result.data.logIn
         }
-        console.log(result)
+       
         return undefined
     }
 
