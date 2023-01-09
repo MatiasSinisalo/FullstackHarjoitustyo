57e7abac37842ddadedb772ed79484521b1d59c2
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-09 13:36:58 +0200

    added option to pass apolloClient to useUserLogIn hook

--- courseApplication/frontend/src/services/logInService.js
+++ courseApplication/frontend/src/services/logInService.js
@@ -1,11 +1,12 @@
 import client from '../client'
 import {LOGIN} from '../queries/userQueries'
 
-export const useUserLogIn = () => {
-   
+export const useUserLogIn = (apolloClient) => {
+    const appClient = apolloClient == undefined ? client : apolloClient
     
     const getToken = async (username, password) => {
-        const result = await client.mutate({mutation: LOGIN, variables: {username, password}})
+        const result = await appClient.mutate({mutation: LOGIN, variables: {username, password}})
+        console.log(result)
         if(result.data.logIn)
         {
             console.log("logged in")
