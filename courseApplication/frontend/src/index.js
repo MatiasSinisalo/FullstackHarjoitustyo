5076cc3d9bf82a2be16698704303714b255db366
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-10 14:16:07 +0200

    added Provider to forward store to App component

--- courseApplication/frontend/src/index.js
+++ courseApplication/frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import client from './client';
+import store from './store';
+import { Provider } from 'react-redux';
 import {  ApolloProvider } from '@apollo/client'
 import {
   BrowserRouter as Router,
@@ -11,7 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <ApolloProvider client={client}>
     <Router>
+      <Provider store={store}>
         <App />
+      </Provider>
     </Router>
   </ApolloProvider>
 );
