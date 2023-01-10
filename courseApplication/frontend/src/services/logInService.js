53aafb0c141c7d420009a0a5586a56dec84f7603
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-10 15:08:01 +0200

    removed duplicate function

--- courseApplication/frontend/src/services/logInService.js
+++ courseApplication/frontend/src/services/logInService.js
@@ -35,16 +35,3 @@ export const useUser = (apolloClient) => {
     return LogInAsUser
     
 } 
-
-export const LogInAsUser = async (username, password, apolloClient) => {
-    const getToken = useUserLogIn(apolloClient)
-    const token = await getToken(username, password)
-    if(token)
-    {
-        localStorage.setItem('courseApplicationUserToken', token.value)
-        const userInfo = await apolloClient.query({query: ME})
-        //console.log(userInfo)
-        return {username: userInfo.data.me.username, name: userInfo.data.me.name, token: token.value}
-    }
-    return null
-}
