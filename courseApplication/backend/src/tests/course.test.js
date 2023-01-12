b8991bb9f50084b18c1ada0e2ddf7107bfacf765
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-12 14:33:45 +0200

    tested that createCourse query saves course correctly to database when parameters are correct

--- courseApplication/backend/src/tests/course.test.js
+++ courseApplication/backend/src/tests/course.test.js
@@ -17,6 +17,10 @@ afterAll(async () => {
     await server.stop()
 })
 
+beforeEach(async () => {
+    await Course.deleteMany({})
+})
+
 describe('course tests', () => {
     describe('course creation tests', () => {
         test('createCourse query returns correctly with correct parameters', async () => {
@@ -32,6 +36,32 @@ describe('course tests', () => {
                 students: []
             }
             expect(createdCourse.data.createCourse).toEqual(correctReturnValue)
+        })
+
+        test('createCourse query saves course to database correctly with correct parameters', async () => {
+            const createdCourse = await apolloServer.executeOperation({query: createCourse, variables: {uniqueName: "uniqueName", name: "common name", teacher: "username"}})
+            
+            const correctReturnValue = {
+                uniqueName: 'uniqueName',
+                name: 'common name',
+                teacher: {
+                    username: 'username',
+                    name: 'name'
+                },
+                students: []
+            }
+
+            //database was cleared before this test, so the database should only have 1 course created
+            const savedCourses = await Course.find({}).populate('teacher')
+            expect(savedCourses.length).toBe(1)
+            
+            const savedCourse = savedCourses[0]
+            expect(savedCourse.uniqueName).toEqual(correctReturnValue.uniqueName)
+            expect(savedCourse.name).toEqual(correctReturnValue.name)
+            expect(savedCourse.teacher.username).toEqual(correctReturnValue.teacher.username)
+            expect(savedCourse.teacher.name).toEqual(correctReturnValue.teacher.name)
+            expect(savedCourse.students).toEqual(correctReturnValue.students)
+
         })
     })
 })
