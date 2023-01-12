a714babe2629f5111fc3074b030ac61ac55abb5c
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-12 11:27:49 +0200

    moved LogIn component test inside describe block

--- courseApplication/frontend/src/tests/LogIn.test.js
+++ courseApplication/frontend/src/tests/LogIn.test.js
@@ -8,24 +8,25 @@ import { ApolloProvider } from '@apollo/client'
 import { createMemoryHistory } from 'history';
 
 
-
-test('login component shows login form correctly', () => {
-    const history = createMemoryHistory({ initialEntries: ["/"]})
-    const containter = render(
-        <ApolloProvider client={client}>
-            <Router history={history}>
-                <Routes>
-                        <Route path="/" element={<LogIn/>}/>
-                </Routes>
-            </Router>
-        </ApolloProvider>
-    )
-    const usernameField = containter.container.querySelector("#usernameInputField")
-    expect(usernameField).not.toBeNull()
+describe('LogIn Component tests', () => {
+    test('login component shows login form correctly', () => {
+        const history = createMemoryHistory({ initialEntries: ["/"]})
+        const containter = render(
+            <ApolloProvider client={client}>
+                <Router history={history}>
+                    <Routes>
+                            <Route path="/" element={<LogIn/>}/>
+                    </Routes>
+                </Router>
+            </ApolloProvider>
+        )
+        const usernameField = containter.container.querySelector("#usernameInputField")
+        expect(usernameField).not.toBeNull()
+        
+        const passwordField = containter.container.querySelector("#passwordInputField")
+        expect(passwordField).not.toBeNull()
     
-    const passwordField = containter.container.querySelector("#passwordInputField")
-    expect(passwordField).not.toBeNull()
-
-    const formSubmitButton = screen.getByText("LogIn")
-    expect(formSubmitButton).toBeDefined()
+        const formSubmitButton = screen.getByText("LogIn")
+        expect(formSubmitButton).toBeDefined()
+    })
 })
