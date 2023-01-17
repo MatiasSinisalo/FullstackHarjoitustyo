821d2e8600af741a1a7141ea099d0d5670fe9f40
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-17 10:17:49 +0200

    CourseBrowser component shows courses stored in local store

--- courseApplication/frontend/src/components/CourseBrowser.js
+++ courseApplication/frontend/src/components/CourseBrowser.js
@@ -1,7 +1,11 @@
+import { useSelector } from "react-redux"
 const CourseBrowser = () =>{
+    const courses = useSelector((store) => store.courses)
     return(
       <>
       <p>Course Browser page</p>
+
+      {courses.map((course) => <p key={course.uniqueName}>{course.uniqueName} {course.name}</p>)}
       </>
   
     )
