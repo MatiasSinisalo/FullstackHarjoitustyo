c9678d59416c406287f722a906af398bd0e53cf4
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-17 09:26:04 +0200

    added a form to create a course

--- courseApplication/frontend/src/components/CreateCourse.js
+++ courseApplication/frontend/src/components/CreateCourse.js
@@ -1,7 +1,23 @@
+
+
+
 const CreateCourse = () =>{
+    const submitCreateCourseForm = (event) => {
+        event.preventDefault()
+        console.log("creating a new course")
+    }
     return(
      <>
      <p>Create course page</p>
+     <form onSubmit={submitCreateCourseForm}>
+        <p>Course unique name</p>
+        <input type="text" name="courseUniqueName"></input>
+        <br></br>
+        <p>Course name</p>
+        <input type="text" name="courseName"></input>
+        <br></br>
+        <input type="submit" value="submit"></input>
+     </form>
      </>
  
    )
