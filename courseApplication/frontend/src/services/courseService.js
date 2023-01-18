4ba5d3859c3afab3d8dbec458e0c90ad8a8db539
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-18 10:00:31 +0200

    fixed error when course creation query fails

--- courseApplication/frontend/src/services/courseService.js
+++ courseApplication/frontend/src/services/courseService.js
@@ -3,7 +3,7 @@ import { CREATE_COURSE } from "../queries/courseQueries"
 export const createCourse = async (uniqueName, name, teacher, apolloClient) => {    
     const createdCourse = await apolloClient.mutate({mutation: CREATE_COURSE, variables: {uniqueName, name, teacher: ""}})
     console.log(createdCourse)
-    if(createdCourse.data.createCourse)
+    if(createdCourse)
     {
         return createdCourse.data.createCourse
     }
