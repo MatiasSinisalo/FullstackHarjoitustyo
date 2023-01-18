21db95ad5678b7f151a51be9e5aec851d8f41045
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-18 10:57:06 +0200

    resetted the test server before and after the tests

--- courseApplication/frontend/cypress/e2e/courseApplication.cy.js
+++ courseApplication/frontend/cypress/e2e/courseApplication.cy.js
@@ -1,3 +1,32 @@
+
+before(async function () {
+    await cy.request({
+        method: 'POST',
+        url: 'http://localhost:4000',
+        body: {
+            query:`
+            mutation Mutation {
+                reset
+            }`,
+            operationName: 'Mutation'
+        }
+    })
+})
+
+after(async function () {
+    await cy.request({
+        method: 'POST',
+        url: 'http://localhost:4000',
+        body: {
+            query:`
+            mutation Mutation {
+                reset
+            }`,
+            operationName: 'Mutation'
+        }
+    })
+})
+
 describe('general app tests', function (){
     it('main page can be visited', function (){
         cy.visit('http://localhost:3000')
