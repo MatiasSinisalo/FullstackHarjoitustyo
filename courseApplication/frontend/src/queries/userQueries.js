8c983458ec0693fcda76554dbdd60912b3e37f37
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-24 09:53:59 +0200

    created a query to create a user

--- courseApplication/frontend/src/queries/userQueries.js
+++ courseApplication/frontend/src/queries/userQueries.js
@@ -9,6 +9,16 @@ mutation LogIn($username: String!, $password: String!) {
   }
 `
 
+export const CREATE_USER = gql`
+mutation Mutation($username: String!, $name: String!, $password: String!) {
+  createUser(username: $username, name: $name, password: $password) {
+    name
+    username
+  }
+}
+`
+
+
 export const ME = gql`
 query Me {
   me {
