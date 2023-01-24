c5862cec8bcb3d9d1771ae610c3ea90d94b3bfad
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-24 13:22:19 +0200

    fixed unhandled error in createCourse

--- courseApplication/frontend/src/services/courseService.js
+++ courseApplication/frontend/src/services/courseService.js
@@ -1,14 +1,19 @@
 import { CREATE_COURSE } from "../queries/courseQueries"
 //teacher field is not currently being used on the backend at all when creating a course
 export const createCourse = async (uniqueName, name, teacher, apolloClient) => {    
-    const createdCourse = await apolloClient.mutate({mutation: CREATE_COURSE, variables: {uniqueName, name, teacher: ""}})
-    console.log(createdCourse)
-    if(createdCourse)
-    {
-        return createdCourse.data.createCourse
+    try{
+        const createdCourse = await apolloClient.mutate({mutation: CREATE_COURSE, variables: {uniqueName, name, teacher: ""}})
+        console.log(createdCourse)
+        if(createdCourse)
+        {
+            return createdCourse.data.createCourse
+        }
+        else{
+            return null
+        }
     }
-    else{
-        return null
+    catch{
+        console.log("Course Creation failed")
     }
 }
 
