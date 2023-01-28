007e9f7d75e256180f251fe6e7e6973effd24710
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-01-28 12:58:30 +0200

    added a test query to create a user with arguments

--- courseApplication/backend/src/tests/userTestQueries.js
+++ courseApplication/backend/src/tests/userTestQueries.js
@@ -1,7 +1,19 @@
+
+//query with hardcoded values to create a user quickly
 const userCreateQuery = 'mutation Mutation {  createUser(password: "12345", name: "name", username: "username") {    name    username  }}'
+
+//query to create a user with arguments
+const createSpesificUserQuery = `mutation CreateUser($createUserUsername2: String!, $name: String!, $createUserPassword2: String!) {
+    createUser(username: $createUserUsername2, name: $name, password: $createUserPassword2) {
+      name
+      username
+    }
+  }
+`
+
 const userLogInQuery = 'mutation LogIn($username: String!, $password: String!) {  logIn(username: $username, password: $password) {   value  }}'
 const allUsersQuery = 'query AllUsers {  allUsers {    name    username  }}'
 const meQuery = 'query Me {  me{    name    username  }}'
 
 
-module.exports = {userCreateQuery, userLogInQuery, allUsersQuery, meQuery}
+module.exports = {userCreateQuery, userLogInQuery, allUsersQuery, meQuery, createSpesificUserQuery}
