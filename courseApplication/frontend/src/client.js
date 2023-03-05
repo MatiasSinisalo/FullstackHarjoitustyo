7f82f0ddd1190e42d1b43b2a2bd17dd8eace0ba8
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-03-05 17:03:53 +0200

    configured frontend to serve the correct backend link depending on the NODE_ENV variable

--- courseApplication/frontend/src/client.js
+++ courseApplication/frontend/src/client.js
@@ -4,9 +4,14 @@ import {
     InMemoryCache,
   } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
+import { RetryLink } from "@apollo/client/link/retry";
+
+//Backendlink is just the server url for backend and its ok that it is on the client side
+
+const backEndURI = process.env.NODE_ENV === "production" ? `https://studastudy-backend.onrender.com` : 'http://localhost:4000'
 
 const httpLink = new HttpLink({
-    uri: 'http://localhost:4000',
+    uri: `https://studastudy-backend.onrender.com` || 'http://localhost:4000',
 })
 
 const tokenHeader = setContext((_, { headers }) => {
