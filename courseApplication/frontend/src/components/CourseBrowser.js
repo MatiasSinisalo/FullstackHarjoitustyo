032e3afd518643ee21cdeeacec5bc0a173d19c57
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-03-12 22:14:56 +0200

    fixed courseBrowser not making a query for all courses

--- courseApplication/frontend/src/components/CourseBrowser.js
+++ courseApplication/frontend/src/components/CourseBrowser.js
@@ -1,7 +1,22 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { useQuery } from "@apollo/client"
+import { GET_ALL_COURSES } from "../queries/courseQueries"
 import CourseShowCase from "./CourseShowCase"
+import { setCourses } from "../reducers/courseReducer"
+import { useEffect } from "react"
 const CourseBrowser = () =>{
+    const dispatch = useDispatch()
+    const allCoursesQuery = useQuery(GET_ALL_COURSES)
+    
     const courses = useSelector((store) => store.courses)
+    useEffect(() => {
+      if(!allCoursesQuery.loading && courses.length === 0)
+      {
+        console.log("set courses due to allCoursesQuery")
+        dispatch(setCourses(allCoursesQuery.data.allCourses))
+      }
+    }, [allCoursesQuery])
+    
     return(
       <>
       <p>Course Browser page</p>
