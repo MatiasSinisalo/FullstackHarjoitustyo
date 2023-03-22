f9542983de3493d05fec6cd1dcb76d2e47f22311
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-03-22 17:28:26 +0200

    added promt in case a course doesnt exist in a given url

--- courseApplication/frontend/src/components/Course.js
+++ courseApplication/frontend/src/components/Course.js
@@ -2,6 +2,7 @@ import { useApolloClient, useQuery } from "@apollo/client"
 import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import {
+  Link,
     useParams
 } from "react-router-dom"
 import { GET_COURSE } from "../queries/courseQueries"
@@ -22,7 +23,14 @@ const Course = () =>{
   {
     return(<p>loading...</p>)
   }
-  const course = courseQuery.data.getCourse
+  const course = courseQuery.data?.getCourse
+  if(!course)
+  {
+    return(<>
+    <h1>Whoops</h1>
+    <Link to='/dashboard'>it seems like this course doesnt exist, click here to go back to dashboard</Link>
+    </>)
+  }
   console.log(course)
   
   return(
