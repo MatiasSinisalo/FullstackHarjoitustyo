8da416d69e417aa7262d5ec1bacd54ff5becfbf8
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-04-04 16:44:22 +0300

    styling to navbar

--- courseApplication/frontend/src/components/NavBar.js
+++ courseApplication/frontend/src/components/NavBar.js
@@ -6,20 +6,18 @@ const NavBar = ({logOut}) => {
     if(user.username)
     {
         return (
-            <div>
+            <div className="navBar">
                 <button onClick={logOut}>Log Out</button>
                 <Link className="navBarLink" to="/dashboard">dashboard</Link>
                 <Link className="navBarLink" to="/CourseBrowser">Courses</Link>
-
-
             </div>
         )
     }
     else
     {
         return (
-            <div>
-                <p>please log in</p>
+            <div className="navBar">
+                <p>please log in <Link to="/">here</Link></p>
             </div>
         )
     }
