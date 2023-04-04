04807c9c8a2580fbcd8531b738a0cf626624883a
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-04-04 16:33:57 +0300

    added some styles to log in view

--- courseApplication/frontend/src/components/LogIn.js
+++ courseApplication/frontend/src/components/LogIn.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-
+import "./styles/login.css"
 const LogIn = ({handleLogIn}) =>{
     const submitLogInForm = async (event) => {
       event.preventDefault()
@@ -10,20 +10,20 @@ const LogIn = ({handleLogIn}) =>{
 
 
     return(
-    <>
+    <div className="loginSection">
       <p>Log in page</p>
   
 
-      <form onSubmit={submitLogInForm}>
-        <input id="usernameInputField" type = "text" name = "username"></input>
+      <form className="loginForm" onSubmit={submitLogInForm}>
+        <input placeholder="username" id="usernameInputField" type = "text" name = "username"></input>
         <br></br>
-        <input id="passwordInputField" type="password" name="password"></input>
+        <input placeholder="password" id="passwordInputField" type="password" name="password"></input>
         <br></br>
         <input type = "submit" value="LogIn"></input>
       </form>
-      <p>dont have an account? create one <Link to="/createAccount">here</Link></p>
+      <p className="createUserPromt">dont have an account? create one <Link to="/createAccount">here</Link></p>
 
-    </>
+    </div>
     )
   }
   export default LogIn
