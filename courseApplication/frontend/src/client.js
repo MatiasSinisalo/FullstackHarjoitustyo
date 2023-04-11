09cbbd79ed20606c1ff8e24e71491b4005b9ac84
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-04-11 14:55:43 +0300

    modified course task cache read to filter out dangling references

--- courseApplication/frontend/src/client.js
+++ courseApplication/frontend/src/client.js
@@ -26,8 +26,23 @@ const tokenHeader = setContext((_, { headers }) => {
     }
 })
 
+//define policy to filter out any dangling references to removed task objects in cache: 
+//https://www.apollographql.com/docs/react/caching/garbage-collection/
+const memoryCache = new InMemoryCache({typePolicies: {
+   Course:{
+        fields: {
+            tasks:{
+                read(tasks){
+                    return tasks == null ? [] : tasks.filter((task) => task != null)
+                }
+            },
+        },
+   },
+}})
+
+
 const client = new ApolloClient({
-    cache: new InMemoryCache(),
+    cache: memoryCache,
     link: tokenHeader.concat(httpLink),
 })
 
