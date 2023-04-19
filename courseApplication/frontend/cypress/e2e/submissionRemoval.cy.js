763c432f0195bdc843c7c0d2a1069a4c6ac47bae
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-04-19 13:39:33 +0300

    created cypress test to check that teacher can remove students task submission

--- courseApplication/frontend/cypress/e2e/submissionRemoval.cy.js
+++ courseApplication/frontend/cypress/e2e/submissionRemoval.cy.js
@@ -1,4 +1,4 @@
-import { prepareTests, endTests, logInAsUser, createCourseAsUser, joinCourseAsUser, createTaskOnCourseAsUser, createSubmissionToATask } from "./helperFunctions.cy"
+import { prepareTests, endTests, logInAsUser, createCourseAsUser, joinCourseAsUser, createTaskOnCourseAsUser, createSubmissionToATask, visitCoursePageAsStudentFromDashboard } from "./helperFunctions.cy"
 
 
 beforeEach(function () {
@@ -9,8 +9,8 @@ beforeEach(function () {
     endTests()
 })
 
-describe('submission removal tests', () => {
-    it('user can remove an created submission', () => {
+describe('submission removal tests teacher', () => {
+    it('teacher can remove an created submission', () => {
         logInAsUser("username", "password1234")
         const course = {
             uniqueName: "this is a course for testing submission removal",
@@ -37,5 +37,38 @@ describe('submission removal tests', () => {
         logInAsUser("username", "password1234")
         cy.contains("See Teachers Course Page").click()
         cy.get('[class*="submission:"]').should('not.exist');
+    })
+
+    it('teacher can remove an created submission made by a student', () => {
+        logInAsUser("username", "password1234")
+        const course = {
+            uniqueName: "this is a course for testing students submission removal",
+            name:  "name of the course"
+        }
+        createCourseAsUser(course.uniqueName, course.name)
+        createTaskOnCourseAsUser(course.uniqueName, "this is a task", new Date(Date.now()))
+       
+        cy.contains("Log Out").click()
+        logInAsUser("second username", "password1234")
+        joinCourseAsUser(course.uniqueName, "second username")
+        visitCoursePageAsStudentFromDashboard(course.uniqueName)
+        createSubmissionToATask("this is a task", "this is an answer by a second user")
+        
+        cy.contains("Log Out").click()
+        logInAsUser("username", "password1234")
+        cy.contains("See Teachers Course Page").click()
+        cy.get(`[class*="submission:"]`).contains("remove").click()
+
+        cy.get(`[class*="submission:"]`).should("not.exist")
+        cy.reload(true)
+        logInAsUser("username", "password1234")
+        cy.contains("See Teachers Course Page").click()
+        cy.get('[class*="submission:"]').should('not.exist');
+
+        cy.contains("Log Out").click()
+        logInAsUser("second username", "password1234")
+        visitCoursePageAsStudentFromDashboard(course.uniqueName)
+        cy.get('[class*="submission:"]').should('not.exist');
+
     })
 })
