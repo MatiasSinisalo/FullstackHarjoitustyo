d94394f31adc3c1e0f1638a3cc49761713a88fa4
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-05-15 15:03:56 +0300

    user can toggle between past tasks view and future tasks view

--- courseApplication/frontend/src/components/TaskListings.js
+++ courseApplication/frontend/src/components/TaskListings.js
@@ -2,19 +2,27 @@ import { useState } from "react"
 import Task from "./Task"
 import './styles/course.css'
 const TaskListings = ({course}) => {
-    const isLate = (task) => {
-        const deadline = new Date(parseInt(task.deadline))
-        return deadline < Date.now()
-    }
-
-    return(
-      <div className="taskListing blueBox">
-      <h2>tasks of the course: </h2>
-      <button>show late tasks</button>
-      <button>show future tasks</button>
-      {course.tasks.map((task) => isLate(task)? <></> : <Task course = {course} task={task} key={task.id}></Task>)}
-      </div>
-    )
+  
+ 
+  
+  const isLate = (task) => {
+    console.log(task)
+    const deadline = new Date(parseInt(task.deadline))
+    return deadline < Date.now()
   }
+  const futureTasks = course.tasks.filter((task) => !isLate(task))
+  const lateTasks = course.tasks.filter((task) => isLate(task))
+  
+  const [displayedTasks, setDisplayedTasks] = useState([])
+
+  return(
+    <div className="taskListing blueBox">
+    <h2>tasks of the course: </h2>
+    <button onClick={() => setDisplayedTasks(lateTasks)}>show past tasks</button>
+    <button onClick={() => setDisplayedTasks(futureTasks)}>show future tasks</button>
+    {displayedTasks.map((task) => <Task course = {course} task={task} key={task.id}></Task>)}
+    </div>
+  )
+}
 
 export default TaskListings
