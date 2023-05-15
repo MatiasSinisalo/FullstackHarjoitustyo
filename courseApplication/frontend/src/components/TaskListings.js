a9647a04f48509e3bc14c0e0748495b90e8ce5d4
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-05-15 16:37:36 +0300

    fix task list not updating when adding / removing tasks

--- courseApplication/frontend/src/components/TaskListings.js
+++ courseApplication/frontend/src/components/TaskListings.js
@@ -1,26 +1,33 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Task from "./Task"
 import './styles/course.css'
 const TaskListings = ({course}) => {
   
- 
-  
   const isLate = (task) => {
-    console.log(task)
     const deadline = new Date(parseInt(task.deadline))
     return deadline < Date.now()
   }
   const futureTasks = course.tasks.filter((task) => !isLate(task))
   const lateTasks = course.tasks.filter((task) => isLate(task))
   
-  const [displayedTasks, setDisplayedTasks] = useState([])
-
+  const displayModes = {
+    future: "future",
+    late: "late"
+  }
+  const [displayMode, setDisplay] = useState(displayModes.future)
+  
   return(
     <div className="taskListing blueBox">
     <h2>tasks of the course: </h2>
-    <button onClick={() => setDisplayedTasks(lateTasks)}>show past tasks</button>
-    <button onClick={() => setDisplayedTasks(futureTasks)}>show future tasks</button>
-    {displayedTasks.map((task) => <Task course = {course} task={task} key={task.id}></Task>)}
+    <button onClick={() => setDisplay(displayModes.late)}>show past tasks</button>
+    <button onClick={() => setDisplay(displayModes.future)}>show future tasks</button>
+    {
+      displayMode == displayModes.future 
+      ?
+        futureTasks.map((task) => <Task course = {course} task={task} key={task.id}></Task>)
+      :
+        lateTasks.map((task) => <Task course = {course} task={task} key={task.id}></Task>)
+    }
     </div>
   )
 }
