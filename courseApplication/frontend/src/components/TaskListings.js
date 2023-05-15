48de27c7298283602b91210c7f10045e0aa38349
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-05-15 14:45:15 +0300

    the course page only lists future tasks

--- courseApplication/frontend/src/components/TaskListings.js
+++ courseApplication/frontend/src/components/TaskListings.js
@@ -1,10 +1,18 @@
+import { useState } from "react"
 import Task from "./Task"
 import './styles/course.css'
 const TaskListings = ({course}) => {
+    const isLate = (task) => {
+        const deadline = new Date(parseInt(task.deadline))
+        return deadline < Date.now()
+    }
+
     return(
       <div className="taskListing blueBox">
       <h2>tasks of the course: </h2>
-      {course.tasks.length > 0 ? course.tasks.map((task) => <Task course = {course} task={task} key={task.id}></Task>) : <></>}
+      <button>show late tasks</button>
+      <button>show future tasks</button>
+      {course.tasks.map((task) => isLate(task)? <></> : <Task course = {course} task={task} key={task.id}></Task>)}
       </div>
     )
   }
