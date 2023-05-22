d7fb31b914de75b80b85107abeb46943ae795e9c
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-05-22 15:25:47 +0300

    add textarea and save button to submission component

--- courseApplication/frontend/src/components/Submission.js
+++ courseApplication/frontend/src/components/Submission.js
@@ -3,13 +3,24 @@ import courseService from "../services/courseService"
 import { useDispatch } from "react-redux"
 import { Notify } from "../reducers/notificationReducer"
 import { removeSubmissionFromTask } from "../reducers/courseReducer"
+import { useState } from "react"
 
 
 const Submission = ({course, task, submission}) => {
     const client = useApolloClient()
     const dispatch = useDispatch()
+    const [submissionContent, setSubmissionContent] = useState(submission.content)
     const removeSubmission = async () => {
         await dispatch(removeSubmissionFromTask(course, task, submission, client))
+    }
+
+    const editSubmission = async () => {
+        console.log("editing submission")
+    }
+
+    const updateSubmissionContent = (event) => {
+        console.log(event.target.value)
+        setSubmissionContent(event.target.value)
     }
 
     const isLate = (task, submission) => {
@@ -26,7 +37,10 @@ const Submission = ({course, task, submission}) => {
     return(
         <div className={`submission:${submission.id}`}>
         {isLate(task, submission) ? <p className="lateMessage">this submission was returned late</p> : <></>}
-        <p>{submission.content}</p>
+        <textarea value={submissionContent} onChange={updateSubmissionContent}></textarea>
+        <br></br>
+        <button onClick={editSubmission}>save</button>
+       
         <p>submitted: {submission.submitted ? <>true</> : <>false</>}</p>
         <button onClick={removeSubmission}>remove</button>
         
