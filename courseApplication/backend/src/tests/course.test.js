af9e88e3a149ddc987c9385e7778f445f65826a8
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-05-24 14:15:05 +0300

    write unit tests for grading a submission

--- courseApplication/backend/src/tests/course.test.js
+++ courseApplication/backend/src/tests/course.test.js
@@ -1530,5 +1530,113 @@ describe('course tests', () => {
             const today = new Date(Date.now()).toISOString().split('T')[0]
             expect(gradingDate).toEqual(today)
         })
+
+        test('grade submission returns course not found if the Course does not exist', async () => {
+            const user = await helpers.logIn("username", apolloServer)
+            const course = await helpers.createCourse("course unique name", "name of course", [], apolloServer)
+            const task = await  helpers.createTask(course, "this is a task", new Date(Date.now()), [], apolloServer)
+            const submission = await helpers.createSubmission(course, task.id, "this is an answer", false, apolloServer)
+
+            const points = 10
+            const gradeSubmissionQuery = await apolloServer.executeOperation({query: gradeSubmission, variables: {courseUniqueName:"this course wont exist", taskId: task.id, submissionId: submission.id, points: points}})
+            expect(gradeSubmissionQuery.errors[0].message).toEqual("Given course not found")
+            console.log(gradeSubmissionQuery)
+            const gradedSubmission = gradeSubmissionQuery.data.gradeSubmission
+            expect(gradedSubmission).toBe(null)
+
+            const courseInDB = await Course.findOne({uniqueName: course.uniqueName})
+            const taskInDB = courseInDB.tasks[0]
+            expect(taskInDB.submissions.length).toBe(1)
+            
+            const submissionInDB = taskInDB.submissions[0]
+            expect(submissionInDB.content).toEqual("this is an answer")
+            expect(submissionInDB.fromUser.toString()).toEqual(user.id)
+            expect(submissionInDB.submitted).toEqual(false)
+            expect(submissionInDB.submittedDate).toEqual(null)
+
+            expect(submissionInDB.grade).toBe(undefined)
+        })
+        
+        test('grade submission returns task not found if the task does not exist', async () => {
+            const user = await helpers.logIn("username", apolloServer)
+            const course = await helpers.createCourse("course unique name", "name of course", [], apolloServer)
+            const task = await  helpers.createTask(course, "this is a task", new Date(Date.now()), [], apolloServer)
+            const submission = await helpers.createSubmission(course, task.id, "this is an answer", false, apolloServer)
+
+            const points = 10
+            const gradeSubmissionQuery = await apolloServer.executeOperation({query: gradeSubmission, variables: {courseUniqueName: course.uniqueName, taskId: "incorrectTaskId", submissionId: submission.id, points: points}})
+            expect(gradeSubmissionQuery.errors[0].message).toEqual("Given task not found")
+            console.log(gradeSubmissionQuery)
+            const gradedSubmission = gradeSubmissionQuery.data.gradeSubmission
+            expect(gradedSubmission).toBe(null)
+
+            const courseInDB = await Course.findOne({uniqueName: course.uniqueName})
+            const taskInDB = courseInDB.tasks[0]
+            expect(taskInDB.submissions.length).toBe(1)
+            
+            const submissionInDB = taskInDB.submissions[0]
+            expect(submissionInDB.content).toEqual("this is an answer")
+            expect(submissionInDB.fromUser.toString()).toEqual(user.id)
+            expect(submissionInDB.submitted).toEqual(false)
+            expect(submissionInDB.submittedDate).toEqual(null)
+
+            expect(submissionInDB.grade).toBe(undefined)
+        })
+
+        test('grade submission returns submission not found if the submission does not exist', async () => {
+            const user = await helpers.logIn("username", apolloServer)
+            const course = await helpers.createCourse("course unique name", "name of course", [], apolloServer)
+            const task = await  helpers.createTask(course, "this is a task", new Date(Date.now()), [], apolloServer)
+            const submission = await helpers.createSubmission(course, task.id, "this is an answer", false, apolloServer)
+
+            const points = 10
+            const gradeSubmissionQuery = await apolloServer.executeOperation({query: gradeSubmission, variables: {courseUniqueName: course.uniqueName, taskId: task.id, submissionId: "incorrectSubmissionId", points: points}})
+            expect(gradeSubmissionQuery.errors[0].message).toEqual("Given submission not found")
+            console.log(gradeSubmissionQuery)
+            const gradedSubmission = gradeSubmissionQuery.data.gradeSubmission
+            expect(gradedSubmission).toBe(null)
+
+            const courseInDB = await Course.findOne({uniqueName: course.uniqueName})
+            const taskInDB = courseInDB.tasks[0]
+            expect(taskInDB.submissions.length).toBe(1)
+            
+            const submissionInDB = taskInDB.submissions[0]
+            expect(submissionInDB.content).toEqual("this is an answer")
+            expect(submissionInDB.fromUser.toString()).toEqual(user.id)
+            expect(submissionInDB.submitted).toEqual(false)
+            expect(submissionInDB.submittedDate).toEqual(null)
+
+            expect(submissionInDB.grade).toBe(undefined)
+        })
+
+        test('grade submission returns unauthorized if the user is not teacher', async () => {
+            const user = await helpers.logIn("username", apolloServer)
+            const course = await helpers.createCourse("course unique name", "name of course", [], apolloServer)
+            const task = await  helpers.createTask(course, "this is a task", new Date(Date.now()), [], apolloServer)
+            const submission = await helpers.createSubmission(course, task.id, "this is an answer", false, apolloServer)
+
+            const anotherUser = await helpers.logIn("students username", apolloServer)
+            await apolloServer.executeOperation({query: addStudentToCourse, variables: {courseUniqueName: "course unique name", addStudentToCourseUsername: "students username"}})
+
+            const points = 10
+            const gradeSubmissionQuery = await apolloServer.executeOperation({query: gradeSubmission, variables: {courseUniqueName: course.uniqueName, taskId: task.id, submissionId: submission.id, points: points}})
+            expect(gradeSubmissionQuery.errors[0].message).toEqual("Unauthorized")
+            console.log(gradeSubmissionQuery)
+            const gradedSubmission = gradeSubmissionQuery.data.gradeSubmission
+            expect(gradedSubmission).toBe(null)
+
+            const courseInDB = await Course.findOne({uniqueName: course.uniqueName})
+            const taskInDB = courseInDB.tasks[0]
+            expect(taskInDB.submissions.length).toBe(1)
+            
+            const submissionInDB = taskInDB.submissions[0]
+            expect(submissionInDB.content).toEqual("this is an answer")
+            expect(submissionInDB.fromUser.toString()).toEqual(user.id)
+            expect(submissionInDB.submitted).toEqual(false)
+            expect(submissionInDB.submittedDate).toEqual(null)
+
+            expect(submissionInDB.grade).toBe(undefined)
+        })
+
     })
 })
