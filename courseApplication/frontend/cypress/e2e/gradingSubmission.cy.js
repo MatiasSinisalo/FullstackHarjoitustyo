ac74230cb508f6dbd82f4cd0f173c53ae7c6e4c5
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-05-30 15:45:49 +0300

    wrote cypress test to check that grade is displayed correctly when maxGrade is available

--- courseApplication/frontend/cypress/e2e/gradingSubmission.cy.js
+++ courseApplication/frontend/cypress/e2e/gradingSubmission.cy.js
@@ -53,4 +53,35 @@ describe('grading a submission tests', () => {
         cy.contains(`date of grading: ${todayString}`)
 
     })
+
+    it('submission displays points / maxGrade if maxGrade is available', () => {
+        logInAsUser("username", "password1234")
+        const course = {uniqueName: "courses unique name", name: "course name"}
+        createCourseAsUser(course.uniqueName, course.name)
+        
+        const task = {description: "this is a task for testing grading", deadline: tomorrow()}
+        createTaskOnCourseAsUser(course.uniqueName, task.description, task.deadline, 10)
+        
+        cy.contains("Log Out").click()
+        cy.wait(50)
+        logInAsUser("second username", "password1234")
+        joinCourseAsUser(course.uniqueName, "second username")
+        visitCoursePageAsStudentFromDashboard(course.uniqueName)
+        visitTaskView(task.description)
+        const submission = {content: "this is an answer to a task"}
+        createSubmissionToATask(task.description, submission.content, false)
+        
+        cy.contains("Log Out").click()
+        cy.wait(50)
+        logInAsUser("username", "password1234")
+        
+        cy.contains("See Teachers Course Page").click()
+        visitTaskView(task.description)
+
+        cy.get(`input[name="points"]`).type("5")
+        cy.get('input[value="submit grade"]').click()
+        
+        cy.contains('this answer got grade: 5 /10')
+
+    })
 })
