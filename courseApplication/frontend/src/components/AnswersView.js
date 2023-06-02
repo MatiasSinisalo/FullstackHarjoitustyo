f3c4efe1b71b7353a0596d85893c382b71e0e6c4
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-06-02 14:23:06 +0300

    changed task view to show submissions in a table

--- courseApplication/frontend/src/components/AnswersView.js
+++ courseApplication/frontend/src/components/AnswersView.js
@@ -6,12 +6,31 @@ const AnswersView = ({course, task}) => {
     return (
         <div className="submissionsListing box">
         <p>answers: </p>
-        {
-            task.submissions.map((submission) => <Submission key={submission.id} course = {course} task={task} submission={submission}></Submission>)
-        }
+        <table>
+            <thead>
+                <tr>
+                    <th>username</th>
+                    <th>submitted</th>
+                    <th>submission</th>
+                </tr>
+            </thead>
+            <tbody>
+                {task.submissions.map((submission) => <SubmissionShowCase  key={submission.id} course = {course} task={task} submission={submission}></SubmissionShowCase>)}
+            </tbody>
+            </table>
         </div>
     )
 }
 
+const SubmissionShowCase = ({course, task, submission}) => {
+    const submittedDate = submission.submitted ? new Date(Number(submission.submittedDate)).toISOString().split("T")[0] : 'not submitted'
+    return (
+        <tr>
+            <td>{submission.fromUser.username}</td>
+            <td>{submittedDate}</td>
+            <td><button onClick={() => console.log(`viewing submission ${submission.id}`)}>view</button></td>
+        </tr>
+    )
+}
 
 export default AnswersView
