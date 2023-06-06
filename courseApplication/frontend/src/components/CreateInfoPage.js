0e7ce043cf3101a2126efe6af32eab3473456384
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-06-06 18:42:17 +0300

    create form for creating an info page

--- courseApplication/frontend/src/components/CreateInfoPage.js
+++ courseApplication/frontend/src/components/CreateInfoPage.js
@@ -1,10 +1,53 @@
+import { useState } from "react"
 
 
 
 const CreateInfoPage = () => {
+    const [blocks, setBlocks] = useState([])
+    const createNewBlock = () => {
+        setBlocks(blocks.concat({position: blocks.length, content: "hello"}))
+    }
+
+    const modifyBlockContent = (newContent, block) => {
+        setBlocks(blocks.map((b) => b.position == block.position ? {...block, content: newContent} : b))
+        console.log(newContent)
+    }
+    const createInfoPage = (event) => {
+        event.preventDefault()
+        console.log(blocks)
+    }
     return(
-        <p>this is a form for info page creation</p>
+       <div onSubmit={createInfoPage}>
+       <form onSubmit={createInfoPage}>
+        <label htmlFor="locationUrl">page location: </label>
+        <input type="text" name="locationUrl"></input>
+        <br/>
+        <p>content blocks: </p>
+        {blocks.map((block) => <Block key={block.position} block={block} modifyBlock={modifyBlockContent}></Block>)} 
+        <br/>
+        <button type="button" onClick={createNewBlock}>new block</button>
+        <br/>
+        <br/>
+        <input type="submit" value="create new info page"></input>
+       </form>
+       
+        <br/>
+       
+       </div>
     )
 }
+
+const Block = ({block, modifyBlock}) => {
+    const whenChanged = (event) => {
+        modifyBlock(event.target.value, block)
+    }
+
+    return(
+        <>
+            <br/>
+            <textarea type="textField" name={`block${block.position}`} value={block.content} onChange={whenChanged}></textarea>
+        </>
+    )
+} 
 
 export default CreateInfoPage
