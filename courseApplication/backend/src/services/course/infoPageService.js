deb2124fc368913c50c37d13957fe42de46b0500
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-06-07 11:57:20 +0300

    fix info pages having duplicate urls

--- courseApplication/backend/src/services/course/infoPageService.js
+++ courseApplication/backend/src/services/course/infoPageService.js
@@ -18,6 +18,12 @@ const addInfoPage = async (uniqueName, locationUrl, userForToken) => {
         throw new UserInputError("Unauthorized")
     }
 
+    const alreadyExistingInfoPage = course.infoPages.find((page) => page.locationUrl === locationUrl)
+    if(alreadyExistingInfoPage)
+    {
+        throw new UserInputError("Given page already exists")
+    }
+
     const infoPage = {
         locationUrl: locationUrl,
         blocks: []
