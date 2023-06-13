c17e757dfd19331a3ad68c0841cfc9afec3cf609
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-06-13 17:18:11 +0300

    write removeChatRoom tests for  user not being teacher, missing course and missing chat room

--- courseApplication/backend/src/tests/course/removeChatRoom.test.js
+++ courseApplication/backend/src/tests/course/removeChatRoom.test.js
@@ -30,6 +30,13 @@ beforeEach(async () => {
 })
 
 
+const checkCourseHasChatRoom = async (chatRoom) => {
+    const coursesInDB = await Course.find({})
+    expect(coursesInDB.length).toBe(1)
+    const courseInDB = coursesInDB[0]
+    expect(courseInDB.chatRooms.length).toBe(1) 
+}
+
 describe('removeChatRoom tests', () => {
     test('removeChatRoom removes chat room correctly', async () => {
         await helpers.logIn("username", apolloServer)
@@ -44,5 +51,42 @@ describe('removeChatRoom tests', () => {
         expect(coursesInDB.length).toBe(1)
         const courseInDB = coursesInDB[0]
         expect(courseInDB.chatRooms.length).toBe(0) 
+    })
+    test('removeChatRoom returns Unauthorized if user is not teacher', async () => {
+        await helpers.logIn("username", apolloServer)
+        const course = await helpers.createCourse("courseUniqueName", "name", [], apolloServer)
+        const chatRoom = await helpers.createChatRoom(course, "room", apolloServer)
+        
+        await helpers.logIn("students username", apolloServer)
+        const removeChatRoomQuery = await apolloServer.executeOperation({query: removeChatRoom, 
+            variables: {courseUniqueName: course.uniqueName, chatRoomId: chatRoom.id}})
+        expect(removeChatRoomQuery.data.removeChatRoom).toBe(null)
+        expect(removeChatRoomQuery.errors[0].message).toBe("Unauthorized")
+
+        await checkCourseHasChatRoom(chatRoom)
+    })
+    test('removeChatRoom returns given course not found if course is not found', async () => {
+        await helpers.logIn("username", apolloServer)
+        const course = await helpers.createCourse("courseUniqueName", "name", [], apolloServer)
+        const chatRoom = await helpers.createChatRoom(course, "room", apolloServer)
+        
+        const removeChatRoomQuery = await apolloServer.executeOperation({query: removeChatRoom, 
+            variables: {courseUniqueName: "doesNotExist", chatRoomId: chatRoom.id}})
+        expect(removeChatRoomQuery.data.removeChatRoom).toBe(null)
+        expect(removeChatRoomQuery.errors[0].message).toBe("Given course not found")
+
+        await checkCourseHasChatRoom(chatRoom)
+    })
+    test('removeChatRoom returns given chatroom not found if chatRoom is not found', async () => {
+        await helpers.logIn("username", apolloServer)
+        const course = await helpers.createCourse("courseUniqueName", "name", [], apolloServer)
+        const chatRoom = await helpers.createChatRoom(course, "room", apolloServer)
+        
+        const removeChatRoomQuery = await apolloServer.executeOperation({query: removeChatRoom, 
+            variables: {courseUniqueName: course.uniqueName, chatRoomId: "abd1234"}})
+        expect(removeChatRoomQuery.data.removeChatRoom).toBe(null)
+        expect(removeChatRoomQuery.errors[0].message).toBe("Given chatroom not found")
+
+        await checkCourseHasChatRoom(chatRoom)
     })
 })
