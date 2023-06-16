6e790b91f42b9b03eedc86c953586f5735ef1dc7
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-06-16 09:50:40 +0300

    change jest startup from globalStartup to setUpFilesAfterEnv

--- courseApplication/backend/src/tests/setup.js
+++ courseApplication/backend/src/tests/setup.js
@@ -1,9 +1,33 @@
 const {startServer} = require('../server')
 const mongoose = require('mongoose')
+const Course = require('../models/course')
+const User = require('../models/user')
+const {Task} = require('../models/task')
+const helpers = require('./testHelpers')
 const config = require('../config')
-module.exports = async function (globalConfig, projectConfig){
-    const server = await startServer()
-    console.log("hello from setup")
-    globalThis.__SERVER__ = server;
-};
+const { userCreateQuery, createSpesificUserQuery } = require('./userTestQueries')
+let server = null
+beforeAll(async () => {
+    server = await startServer()
+    
+    await mongoose.connect(config.MONGODB_URI)
+    await Course.deleteMany({})
+    await User.deleteMany({})
+    await helpers.makeQuery({query: userCreateQuery, variables: {}})
+    await helpers.makeQuery({query: createSpesificUserQuery, variables:{username: "students username", name: "students name", password: "12345"}})
+})
+
+afterAll(async () => {
+    await server.apolloServer.stop();
+    await server.httpServer.close();
+    await Course.deleteMany({})
+    await User.deleteMany({})
+    await mongoose.connection.close()
+})
+
+beforeEach(async () => {
+    await Course.deleteMany({})
+    await Task.deleteMany({})
+    helpers.logOut()
+})
 
