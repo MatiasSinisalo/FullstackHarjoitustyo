272642cf1a0ff7d63d02baad2149d43870ce50aa
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-06-16 08:12:13 +0300

    fix addSubmission tests

--- courseApplication/backend/src/tests/course/addSubmission.test.js
+++ courseApplication/backend/src/tests/course/addSubmission.test.js
@@ -10,13 +10,13 @@ const { query } = require('express')
 const mongoose = require('mongoose')
 const course = require('../../models/course')
 const helpers = require('../testHelpers')
-
+const config = require('../../config')
 beforeAll(async () => {
     await mongoose.connect(config.MONGODB_URI)
     await Course.deleteMany({})
     await User.deleteMany({})
-    await request(url).post("/").send({query: userCreateQuery, variables: {}})
-    await request(url).post("/").send({query: createSpesificUserQuery, variables:{username: "students username", name: "students name", password: "12345"}})
+    await request(config.LOCAL_SERVER_URL).post("/").send({query: userCreateQuery, variables: {}})
+    await request(config.LOCAL_SERVER_URL).post("/").send({query: createSpesificUserQuery, variables:{username: "students username", name: "students name", password: "12345"}})
 })
 
 afterAll(async () => {
@@ -35,18 +35,17 @@ beforeEach(async () => {
 
 describe('addSubmissionToCourseTask tests', () => {
     test('user can create a submission to a task', async () => {
-        const userQuery = await User.findOne({username: "username"})
+        const userQuery = await helpers.logIn("username", "12345")
         const userid = userQuery._id.toString()
-        apolloServer.context = {userForToken: {username: "username", name:"name", id: userid}}
         const course = {uniqueName: "course-owned-by-username", name: "common name", teacher: "username", tasks: []}
-        const createdCourse = await apolloServer.executeOperation({query: createCourse, variables: course})
+        const createdCourse = await helpers.makeQuery({query: createCourse, variables: course})
         const task = {
             description:  "this is the description of a task that is about testing",
             deadline: new Date("2030-06-25"),
             submissions: []
         }
        
-        const createdTask = await apolloServer.executeOperation({query: addTaskToCourse, variables: {courseUniqueName: course.uniqueName, description: task.description, deadline: task.deadline.toString()}});
+        const createdTask = await helpers.makeQuery({query: addTaskToCourse, variables: {courseUniqueName: course.uniqueName, description: task.description, deadline: task.deadline.toString()}});
         const taskID = createdTask.data.addTaskToCourse.id
 
         const submission = {
@@ -55,7 +54,7 @@ describe('addSubmissionToCourseTask tests', () => {
             taskId: taskID
         }
         
-        const response = await apolloServer.executeOperation({query: addSubmissionToCourseTask, 
+        const response = await helpers.makeQuery({query: addSubmissionToCourseTask, 
             variables: {
             courseUniqueName: course.uniqueName, 
             taskId: submission.taskId,
@@ -85,18 +84,18 @@ describe('addSubmissionToCourseTask tests', () => {
         expect(new Date(courseInDB.tasks[0].submissions[0].submittedDate).toISOString().slice(0, 16)).toEqual(new Date(Date.now()).toISOString().slice(0, 16))
     })
     test('user can create a submission to a task only once', async () => {
-        const userQuery = await User.findOne({username: "username"})
+        const userQuery = await helpers.logIn("username", "12345")
         const userid = userQuery._id.toString()
-        apolloServer.context = {userForToken: {username: userQuery.username, name: userQuery.name, id: userid, _id: userQuery._id}}
+      
         const course = {uniqueName: "course-owned-by-username", name: "common name", teacher: "username", tasks: []}
-        const createdCourse = await apolloServer.executeOperation({query: createCourse, variables: course})
+        const createdCourse = await helpers.makeQuery({query: createCourse, variables: course})
         const task = {
             description:  "this is the description of a task that is about testing",
             deadline: new Date("2030-06-25"),
             submissions: []
         }
        
-        const createdTask = await apolloServer.executeOperation({query: addTaskToCourse, variables: {courseUniqueName: course.uniqueName, description: task.description, deadline: task.deadline.toString()}});
+        const createdTask = await helpers.makeQuery({query: addTaskToCourse, variables: {courseUniqueName: course.uniqueName, description: task.description, deadline: task.deadline.toString()}});
         const taskID = createdTask.data.addTaskToCourse.id
 
         const submission = {
@@ -105,7 +104,7 @@ describe('addSubmissionToCourseTask tests', () => {
             taskId: taskID
         }
         
-        const response = await apolloServer.executeOperation({query: addSubmissionToCourseTask, 
+        const response = await helpers.makeQuery({query: addSubmissionToCourseTask, 
             variables: {
             courseUniqueName: course.uniqueName, 
             taskId: submission.taskId,
@@ -123,7 +122,7 @@ describe('addSubmissionToCourseTask tests', () => {
             submitted: true,
             taskId: taskID
         }
-        const secondResponse = await apolloServer.executeOperation({query: addSubmissionToCourseTask, variables: {
+        const secondResponse = await helpers.makeQuery({query: addSubmissionToCourseTask, variables: {
             courseUniqueName: course.uniqueName,
             taskId: secondSubmission.taskId,
             content: secondSubmission.content,
