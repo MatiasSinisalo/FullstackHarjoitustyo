eedfaaabfe077c097f5dd4caf3cfbf712347ceb5
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-06-16 08:59:19 +0300

    fix user tests

--- courseApplication/backend/src/tests/user.test.js
+++ courseApplication/backend/src/tests/user.test.js
@@ -1,11 +1,14 @@
 
-//source for testing an application that uses graphQL:  https://www.apollographql.com/docs/apollo-server/testing/testing/
-const {server, apolloServer} = require('../server')
-const testServer = apolloServer
+//source for testing an application that uses graphQL:  https://www.apollographql.com/docs/apollo-server/testing/testing
 const request = require('supertest')
 const context = require('../context')
 const User = require('../models/user')
-const {userCreateQuery, userLogInQuery, allUsersQuery, meQuery} = require('./userTestQueries')
+const {userCreateQuery, userLogInQuery, allUsersQuery, meQuery, createSpesificUserQuery} = require('./userTestQueries')
+const mongoose = require('mongoose')
+const {Task} = require('../models/task')
+const config = require('../config')
+const helpers = require('./testHelpers')
+const Course = require('../models/course')
 
 beforeAll(async () => {
     await mongoose.connect(config.MONGODB_URI)
@@ -21,17 +24,17 @@ afterAll(async () => {
 })
 
 beforeEach(async () => {
+    await User.deleteMany({})
     await Course.deleteMany({})
     await Task.deleteMany({})
     helpers.logOut()
 })
 
-
 describe('user tests', () => {
 
     test('user create mutation returns and saves created user correctly', async () => {  
        
-        const response = await testServer.executeOperation({query: userCreateQuery, variables: {}})
+        const response = await helpers.makeQuery({query: userCreateQuery, variables: {}})
       
         expect(response.errors).toBeUndefined();
         expect(response.data.createUser).toEqual({name: 'name', username: 'username'})
@@ -44,11 +47,11 @@ describe('user tests', () => {
     describe('log in tests', () => {
         test('logIn mutation with correct credentials returns a token', async () => {  
         
-            const response = await testServer.executeOperation({query: userCreateQuery, variables: {}})
+            const response = await helpers.makeQuery({query: userCreateQuery, variables: {}})
           
             expect(response.errors).toBeUndefined();
             
-            const logInResponse = await testServer.executeOperation({query: userLogInQuery, variables: {username: "username", password: "12345"}})
+            const logInResponse = await helpers.makeQuery({query: userLogInQuery, variables: {username: "username", password: "12345"}})
             expect(logInResponse.errors).toBeUndefined();
             expect(logInResponse.data.logIn).toBeDefined();
             
@@ -56,26 +59,26 @@ describe('user tests', () => {
 
         test('logIn mutation with incorrect password returns a null token and an error', async () => {  
         
-            const response = await testServer.executeOperation({query: userCreateQuery, variables: {}})
+            const response = await helpers.makeQuery({query: userCreateQuery, variables: {}})
           
             expect(response.errors).toBeUndefined();
             
-            const logInResponse = await testServer.executeOperation({query: userLogInQuery, variables: {username: "username", password: "wrong"}})
+            const logInResponse = await helpers.makeQuery({query: userLogInQuery, variables: {username: "username", password: "wrong"}})
           
-            expect(logInResponse.errors.toString()).toContain("invalid username or password");
+            expect(logInResponse.errors[0].message).toContain("invalid username or password");
             expect(logInResponse.data.logIn).toBeDefined();
             
         })
 
         test('logIn mutation with incorrect username returns a null token and an error', async () => {  
         
-            const response = await testServer.executeOperation({query: userCreateQuery, variables: {}})
+            const response = await helpers.makeQuery({query: userCreateQuery, variables: {}})
             
             expect(response.errors).toBeUndefined();
             
-            const logInResponse = await testServer.executeOperation({query: userLogInQuery, variables: {username: "username3", password: "12345"}})
+            const logInResponse = await helpers.makeQuery({query: userLogInQuery, variables: {username: "username3", password: "12345"}})
            
-            expect(logInResponse.errors.toString()).toContain("invalid username or password");
+            expect(logInResponse.errors[0].message).toContain("invalid username or password");
             expect(logInResponse.data.logIn).toBeDefined();
             
         })
@@ -83,8 +86,8 @@ describe('user tests', () => {
 
     describe('authorization tests', () => {
         test('query "Me" returns user information if the token is valid', async () => {
-            const response = await testServer.executeOperation({query: userCreateQuery, variables: {}})
-            const logInResponse = await testServer.executeOperation({query: userLogInQuery, variables: {username: "username", password: "12345"}})
+            const response = await helpers.makeQuery({query: userCreateQuery, variables: {}})
+            const logInResponse = await helpers.makeQuery({query: userLogInQuery, variables: {username: "username", password: "12345"}})
             const token = logInResponse.data.logIn
             const authorization = `bearer ${token.value.toString()}`
            // console.log(authorization)
@@ -109,8 +112,8 @@ describe('user tests', () => {
         })
 
         test('query "Me" returns null if invalid token is given', async () => {
-            const response = await testServer.executeOperation({query: userCreateQuery, variables: {}})
-            const logInResponse = await testServer.executeOperation({query: userLogInQuery, variables: {username: "username", password: "12345"}})
+            const response = await helpers.makeQuery({query: userCreateQuery, variables: {}})
+            const logInResponse = await helpers.makeQuery({query: userLogInQuery, variables: {username: "username", password: "12345"}})
             const token = logInResponse.data.logIn
             
             const authorization = `bearer ${token.value.toString()}abc`
@@ -127,9 +130,5 @@ describe('user tests', () => {
     })
 })
 
-afterAll(async () => {
-    //we use different database for tests, lets clear the database
-    await User.deleteMany({})
-    await server.stop()
-})
+
 
