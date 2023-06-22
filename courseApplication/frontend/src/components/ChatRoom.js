904f03e8ebf1eb818b13e09aedc0d723cf991e33
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-06-22 17:13:40 +0300

    update local cache when new message is created

--- courseApplication/frontend/src/components/ChatRoom.js
+++ courseApplication/frontend/src/components/ChatRoom.js
@@ -3,16 +3,29 @@ import Messages from "./Messages"
 import CreateMessage from "./CreateMessage"
 import AddUsersToChatRoom from "./AddUsersToChatRoom"
 import ChatRoomUsers from "./ChatRoomUsers"
-import { useSubscription } from "@apollo/client"
+import { useApolloClient, useSubscription } from "@apollo/client"
 import { SUBSCRIBE_TO_MESSAGE_CREATED } from "../queries/courseQueries"
 
 const ChatRoom = ({course, user}) => {
     const chatRoomId = useParams().chatRoomId
     const chatRoom = course.chatRooms.find((room) => room.id === chatRoomId)
+    const client = useApolloClient()
     useSubscription(SUBSCRIBE_TO_MESSAGE_CREATED, {
         variables: {courseUniqueName: course.uniqueName, chatRoomId: chatRoom?.id},
         onData({data}){
             console.log(data)
+            const messageCreated = data.data.messageCreated
+            //TODO: maybe sort messages to avoid wrong order?
+            client.cache.modify(
+            {
+                id: `ChatRoom:${chatRoom.id}`,
+                fields: {
+                    messages(currentMessages){
+                        console.log(messageCreated)
+                        return currentMessages.concat(messageCreated)
+                    }
+                }
+            })
         }
     })
   
