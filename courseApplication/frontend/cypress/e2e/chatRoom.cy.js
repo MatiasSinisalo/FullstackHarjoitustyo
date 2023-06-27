c2bd353ac119d950796d2add8cf19ea9bbb53a3e
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-06-27 15:32:57 +0300

    write cypress test for chatRoom removal

--- courseApplication/frontend/cypress/e2e/chatRoom.cy.js
+++ courseApplication/frontend/cypress/e2e/chatRoom.cy.js
@@ -77,6 +77,30 @@ describe('chatroom tests', () => {
         cy.get("a").contains("chatRoomName").click()
         cy.get("h1").contains("chatRoomName")
     })
+    it('room can be removed by teacher', function() {
+        logInAsUser("username", "password1234")
+        createCourseAsUser("courseUniqueName", "name")
+        
+        createChatRoom("chatRoomName")
+        cy.visit('http://localhost:3000/dashboard', {onBeforeLoad(win) {
+            cy.stub(win, 'prompt').returns("chatRoomName")
+        }})
+        visitChatRoom("chatRoomName")
+        catchRequestAs("removeChatRoom")
+        cy.get("button").contains("remove chatRoom").click()
+        cy.wait("@removeChatRoom").then((comms) => {
+            const variables = comms.request.body.variables
+            expect(variables.courseUniqueName).to.equal("courseUniqueName")
+        })
+
+        cy.contains("it seems this chatRoom does not exist, click here to go to course page")
+
+        cy.reload()
+        logInAsUser("username", "password1234")
+        cy.contains("dashboard").click()
+        cy.contains("See Teachers Course Page").click()
+        cy.get("a").contains("chatRoomName").should("not.exist")
+    })
     it('teacher can add student to room', function() {
         logInAsUser("username", "password1234")
         createCourseAsUser("courseUniqueName", "name")
