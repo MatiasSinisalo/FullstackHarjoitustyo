e23f274bc9c66373e3ae8e7d3c6e445d34585daa
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-07-03 14:25:49 +0300

    new messages appear correctly in the scrollable area

--- courseApplication/frontend/src/components/Messages.js
+++ courseApplication/frontend/src/components/Messages.js
@@ -1,10 +1,13 @@
 import "./styles/course.css"
 const Messages = ({course, chatRoom}) =>{
+    console.log("re rendered messages!")
     return(
       <>
       <p>messages: </p>
-      <div className="course-messages scrollable">
-        {chatRoom.messages.map((msg) => <p key={msg.id}>{msg.content} by: {msg.fromUser.username}</p>)}
+      <div onScroll={() => console.log("scrolled!")} className="scrollable course-messages">
+        <div className="course-messages-content">
+          {chatRoom.messages.map((msg) => <p key={msg.id}>{msg.content} by: {msg.fromUser.username}</p>)}
+        </div>
       </div>
       </>
   
