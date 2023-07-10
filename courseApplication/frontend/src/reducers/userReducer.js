997324c38210a144a01fa12b4a9cf9fecab7e0a8
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-07-10 15:56:17 +0300

    removed userReducer state

--- courseApplication/frontend/src/reducers/userReducer.js
+++ courseApplication/frontend/src/reducers/userReducer.js
@@ -1,21 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { createUser } from '../services/logInService'
 
-const nullUser = {username: null, name: null, token: null}
-
-const userSlice = createSlice({
-  name: 'user',
-  initialState: nullUser,
-  reducers: {
-    updateUser (state, action){
-        return action.payload
-    }
-  }
-})
-
-
-export const {updateUser} = userSlice.actions
-
 export const createNewUser = (username, name, password, client) => {
   return async function (dispatch){
     console.log("creating a new user: ")
@@ -23,8 +8,3 @@ export const createNewUser = (username, name, password, client) => {
     return userdata
   }
 }
-
-
-
-
-export default userSlice.reducer
