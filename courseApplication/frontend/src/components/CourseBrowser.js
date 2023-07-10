b1f848d4e4fd536e059ff5ad28b8bc89a5209355
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-07-10 13:52:53 +0300

    fix courseBrowser not displaying courseShowcases

--- courseApplication/frontend/src/components/CourseBrowser.js
+++ courseApplication/frontend/src/components/CourseBrowser.js
@@ -4,24 +4,27 @@ import { GET_ALL_COURSES } from "../queries/courseQueries"
 import CourseShowCase from "./CourseShowCase"
 import { getAllCourses, setCourses } from "../reducers/courseReducer"
 import { useEffect } from "react"
+import { ME } from "../queries/userQueries"
 const CourseBrowser = () =>{
     const dispatch = useDispatch()
     const client = useApolloClient()
     
+    const userQuery = useQuery(ME)
+    
     const allCoursesQuery = useQuery(GET_ALL_COURSES)
-    if(allCoursesQuery.loading)
+    if(allCoursesQuery.loading || userQuery.loading)
     {
       return (<p>loading...</p>)
     }
     const courses = allCoursesQuery.data.allCourses
-   
+    const user = userQuery.data.me
     
     return(
       <div className="container">
         
         <div className="container ">
           <h1>Course Browser page</h1>
-          {courses.map((course) => <CourseShowCase key={course.uniqueName} course={course}></CourseShowCase>)}
+          {courses.map((course) => <CourseShowCase key={course.uniqueName} currentUser={user} course={course}></CourseShowCase>)}
         </div>
       </div>
   
