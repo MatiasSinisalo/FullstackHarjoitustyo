fca96f952c2d5a1b929719e60ff0815894aa0478
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2023-07-12 18:44:25 +0300

    add test to check that user can navigate from calendar tasks to the task view

--- courseApplication/frontend/cypress/e2e/calendar.cy.js
+++ courseApplication/frontend/cypress/e2e/calendar.cy.js
@@ -13,23 +13,26 @@ const visitCalendar = () => {
 }
 
 describe('Calendar tests', () => {
+    const courseUniqueName = "course-uniqueName"
+    const task1Description = "this is a task"  
 
-    it('Calendar displays tasks from all courses that the user participates in', () => {
-        logInAsUser("username", "password1234")
+    const secondCourseUniqueName = "second-course-uniqueName"
+    const task2Description = "this is the second task"
 
-        const courseUniqueName = "course-uniqueName"
-        const task1Description = "this is a task"  
+    beforeEach(function () {
+        logInAsUser("username", "password1234")       
         createCourseAsUser(courseUniqueName, "name")
         createTaskOnCourseAsUser(courseUniqueName, task1Description, tomorrow(), 5);
 
         cy.contains("dashboard").click()
 
-        const secondCourseUniqueName = "second-course-uniqueName"
-        const task2Description = "this is the second task"
         createCourseAsUser(secondCourseUniqueName, "name")
         createTaskOnCourseAsUser(secondCourseUniqueName, task2Description, tomorrow(), 5);
 
         cy.contains("Log Out").click()
+    })
+    
+    it('Calendar displays tasks from all courses that the user participates in', () => {
         logInAsUser("second username", "password1234")
         joinCourseAsUser(courseUniqueName, "second username")
         joinCourseAsUser(secondCourseUniqueName, "second username")
@@ -38,5 +41,20 @@ describe('Calendar tests', () => {
 
         cy.get('div[class*="day"]').contains(`tasks: 1, ${courseUniqueName}`)
         cy.get('div[class*="day"]').contains(`tasks: 1, ${secondCourseUniqueName}`)
+    })
+
+    it('user can navigate from Calendar tasks popup to task view', () => {
+        logInAsUser("second username", "password1234")
+        joinCourseAsUser(courseUniqueName, "second username")
+
+        visitCalendar()
+
+        cy.get('div[class*="day"]').contains(`tasks: 1, ${courseUniqueName}`).click()
+        cy.contains(task1Description).click()
+        
+        cy.contains(task1Description)
+        cy.contains(courseUniqueName)
+        cy.contains('max grade')
+        cy.contains('create new solution')
     })
 })
