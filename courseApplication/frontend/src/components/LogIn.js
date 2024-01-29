491dfaf91039cf9c6181f72e1710b69860a48c98
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2024-01-29 15:02:52 +0200

    write component for HY Test IdP login

--- courseApplication/frontend/src/components/LogIn.js
+++ courseApplication/frontend/src/components/LogIn.js
@@ -28,6 +28,20 @@ const GoogleLogin = ({}) => {
 }
 
 
+const HYLogin = ({}) => {
+  const client_id = 'id_af7f822a95ec6e8b8316268f679b9aa6'
+  const redirect_uri = 'https://frontend-ohtuprojekti-staging.ext.ocp-test-0.k8s.it.helsinki.fi/';
+
+
+  return(
+    <>
+      <a href={`https://login-test.it.helsinki.fi/idp/profile/oidc/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=openid%20profile`}>
+        log in using HY
+      </a>
+    </>
+  )
+}
+
 
 
 const LogIn = ({handleLogIn}) =>{
@@ -54,6 +68,7 @@ const LogIn = ({handleLogIn}) =>{
         </form>
         <p className="create-user-promt">dont have an account? create one <Link to="/createAccount">here</Link></p>
         <GoogleLogin></GoogleLogin>
+        <HYLogin></HYLogin>
       </div>
       </div>
     </div>
