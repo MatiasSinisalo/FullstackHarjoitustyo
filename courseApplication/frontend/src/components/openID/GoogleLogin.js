151115dc9ae787b6e4e67b28346cb4eb347e11cc
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2024-01-29 15:25:01 +0200

    remove code from GoogleLogin component

--- courseApplication/frontend/src/components/openID/GoogleLogin.js
+++ courseApplication/frontend/src/components/openID/GoogleLogin.js
@@ -5,17 +5,6 @@ import { useApolloClient } from "@apollo/client";
 import { useState } from "react";
 
 const GoogleLogin = ({}) => {
-    const [searchParams, setSearchParams] = useSearchParams();
-   
-    const param = searchParams.get("code")
-    const client = useApolloClient()
-    const navigate = useNavigate()
-   
-    if(param != null){
-      // send code to backend to then get the user id token
-      authenticateGoogleUser(param, client, navigate)
-      return("logging in with google...")
-    }
     const client_id = '526000597568-srhi8bscvhvi2s7br23bomf7vvgrskkr.apps.googleusercontent.com'
     const redirect_uri = 'https://frontend-ohtuprojekti-staging.ext.ocp-test-0.k8s.it.helsinki.fi/login/google';
   
