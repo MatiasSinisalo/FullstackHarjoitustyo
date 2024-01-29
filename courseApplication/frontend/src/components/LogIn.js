c1eb8d0e0ba5e8ac9839a125b3b28af679bd61fa
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2024-01-29 15:11:10 +0200

    move google log in to its own file

--- courseApplication/frontend/src/components/LogIn.js
+++ courseApplication/frontend/src/components/LogIn.js
@@ -1,32 +1,9 @@
-import { Link, useNavigate, useParams, useSearchParams } from "react-router-dom"
+import { Link, redirect, useNavigate, useParams, useSearchParams } from "react-router-dom"
 import "./styles/login.css"
 import { authenticateGoogleUser } from "../reducers/userReducer";
 import { useApolloClient } from "@apollo/client";
-
-const GoogleLogin = ({}) => {
-  const [searchParams, setSearchParams] = useSearchParams();
- 
-  const param = searchParams.get("code")
-  const client = useApolloClient()
-  const navigate = useNavigate()
- 
-  if(param != null){
-    // send code to backend to then get the user id token
-    authenticateGoogleUser(param, client, navigate)
-    return("logging in with google...")
-  }
-  const client_id = '526000597568-srhi8bscvhvi2s7br23bomf7vvgrskkr.apps.googleusercontent.com'
-  const redirect_uri = 'https://frontend-ohtuprojekti-staging.ext.ocp-test-0.k8s.it.helsinki.fi/';
-
-  return(
-    <>
-      <a href={`https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=openid%20email%20profile`}
-      >login using google</a>
-      
-    </>
-  )
-}
-
+import { useState } from "react";
+import GoogleLogin from "./openID/GoogleLogin";
 
 const HYLogin = ({}) => {
   const client_id = 'id_af7f822a95ec6e8b8316268f679b9aa6'
